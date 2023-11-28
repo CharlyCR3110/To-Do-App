b38f836ac4654fc2f87f1714131aa4d7a4b249d9
Charly <carlosgarita3110@gmail.com>
2023-11-27 19:44:10 -0600

    Add useEffect hook to fetch tasks and addTask function to create new tasks

--- FrontEnd/src/components/TaskList.jsx
+++ FrontEnd/src/components/TaskList.jsx
@@ -1,24 +1,30 @@
-// TaskList.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TaskItem } from "./TaskItem";
 import { TaskCreationBar } from "./TaskCreationBar";
 
 export function TaskList() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      description: "Example task",
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Realiza una solicitud para obtener las tareas al cargar el componente
+    fetch("http://localhost:3000/tasks")
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch((error) => console.error("Error fetching tasks:", error));
+  }, []); // El segundo parámetro [] asegura que el efecto se ejecute solo una vez al montar el componente.
 
   const addTask = (newTask) => {
-    setTasks((prevTasks) => [
-      ...prevTasks,
-      {
-        id: prevTasks.length + 1,
-        description: newTask,
+    // Realiza una solicitud para crear una nueva tarea
+    fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
       },
-    ]);
+      body: JSON.stringify({ description: newTask }),
+    })
+      .then((response) => response.json())
+      .then((data) => setTasks((prevTasks) => [...prevTasks, data]))
+      .catch((error) => console.error("Error creating task:", error));
   };
 
   return (
